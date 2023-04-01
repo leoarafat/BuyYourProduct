7a624951984bbf0564aa517e7c7047c79af24cdb
leoarafat10 <97153759+leoarafat10@users.noreply.github.com>
2023-04-01 13:05:05 +0800

    complete filtering

--- components/layouts/Filters.jsx
+++ components/layouts/Filters.jsx
@@ -1,16 +1,60 @@
-'use client'
-import React from "react";
+"use client";
+
+import React, { useState } from "react";
 import StarRatings from "react-star-ratings";
+import { useRouter } from "next/navigation";
+import { getPriceQueryParams } from "@/helpers/helpers";
 
 const Filters = () => {
+  const [min, setMin] = useState("");
+  const [max, setMax] = useState("");
+
+  const router = useRouter();
+
   let queryParams;
 
-  function checkHandler(checkBoxType, checkBoxValue) {
+  function handleClick(checkbox) {
     if (typeof window !== "undefined") {
       queryParams = new URLSearchParams(window.location.search);
     }
 
+    const checkboxes = document.getElementsByName(checkbox.name);
+
+    checkboxes.forEach((item) => {
+      if (item !== checkbox) item.checked = false;
+    });
+
+    if (checkbox.checked === false) {
+      // Delete the filter from query
+      queryParams.delete(checkbox.name);
+    } else {
+      // Set filter in the query
+      if (queryParams.has(checkbox.name)) {
+        queryParams.set(checkbox.name, checkbox.value);
+      } else {
+        queryParams.append(checkbox.name, checkbox.value);
+      }
+    }
+    const path = window.location.pathname + "?" + queryParams.toString();
+    router.push(path);
+  }
+
+  function handleButtonClick() {
     if (typeof window !== "undefined") {
+      queryParams = new URLSearchParams(window.location.search);
+
+      queryParams = getPriceQueryParams(queryParams, "min", min);
+      queryParams = getPriceQueryParams(queryParams, "max", max);
+
+      const path = window.location.pathname + "?" + queryParams.toString();
+      router.push(path);
+    }
+  }
+
+  function checkHandler(checkBoxType, checkBoxValue) {
+    if (typeof window !== "undefined") {
+      queryParams = new URLSearchParams(window.location.search);
+
       const value = queryParams.get(checkBoxType);
       if (checkBoxValue === value) return true;
       return false;
@@ -21,7 +65,7 @@ const Filters = () => {
     <aside className="md:w-1/3 lg:w-1/4 px-4">
       <a
         className="md:hidden mb-5  w-full text-center px-4 py-2 inline-block text-lg text-gray-700 bg-white shadow-sm border border-gray-200 rounded-md hover:bg-gray-100 hover:text-blue-600"
-        href=""
+        href="#"
       >
         Filter by
       </a>
@@ -34,6 +78,8 @@ const Filters = () => {
               className="appearance-none border border-gray-200 bg-gray-100 rounded-md py-2 px-3 hover:border-gray-400 focus:outline-none focus:border-gray-400 w-full"
               type="number"
               placeholder="Min"
+              value={min}
+              onChange={(e) => setMin(e.target.value)}
             />
           </div>
 
@@ -43,11 +89,16 @@ const Filters = () => {
               className="appearance-none border border-gray-200 bg-gray-100 rounded-md py-2 px-3 hover:border-gray-400 focus:outline-none focus:border-gray-400 w-full"
               type="number"
               placeholder="Max"
+              value={max}
+              onChange={(e) => setMax(e.target.value)}
             />
           </div>
 
           <div className="mb-4">
-            <button className="px-1 py-2 text-center w-full inline-block text-white bg-blue-600 border border-transparent rounded-md hover:bg-blue-700">
+            <button
+              className="px-1 py-2 text-center w-full inline-block text-white bg-blue-600 border border-transparent rounded-md hover:bg-blue-700"
+              onClick={handleButtonClick}
+            >
               Go
             </button>
           </div>
@@ -66,6 +117,7 @@ const Filters = () => {
                 value="Electronics"
                 className="h-4 w-4"
                 defaultChecked={checkHandler("category", "Electronics")}
+                onClick={(e) => handleClick(e.target)}
               />
               <span className="ml-2 text-gray-500"> Electronics </span>
             </label>
@@ -78,6 +130,7 @@ const Filters = () => {
                 value="Laptops"
                 className="h-4 w-4"
                 defaultChecked={checkHandler("category", "Laptops")}
+                onClick={(e) => handleClick(e.target)}
               />
               <span className="ml-2 text-gray-500"> Laptops </span>
             </label>
@@ -90,6 +143,7 @@ const Filters = () => {
                 value="Toys"
                 className="h-4 w-4"
                 defaultChecked={checkHandler("category", "Toys")}
+                onClick={(e) => handleClick(e.target)}
               />
               <span className="ml-2 text-gray-500"> Toys </span>
             </label>
@@ -102,6 +156,7 @@ const Filters = () => {
                 value="Office"
                 className="h-4 w-4"
                 defaultChecked={checkHandler("category", "Office")}
+                onClick={(e) => handleClick(e.target)}
               />
               <span className="ml-2 text-gray-500"> Office </span>
             </label>
@@ -111,11 +166,12 @@ const Filters = () => {
               <input
                 name="category"
                 type="checkbox"
-                value="Beauty"
+                value="Headphones"
                 className="h-4 w-4"
-                defaultChecked={checkHandler("category", "Beauty")}
+                defaultChecked={checkHandler("category", "Headphones")}
+                onClick={(e) => handleClick(e.target)}
               />
-              <span className="ml-2 text-gray-500"> Beauty </span>
+              <span className="ml-2 text-gray-500"> Headphones </span>
             </label>
           </li>
         </ul>
@@ -133,6 +189,7 @@ const Filters = () => {
                   value={rating}
                   className="h-4 w-4"
                   defaultChecked={checkHandler("ratings", `${rating}`)}
+                  onClick={(e) => handleClick(e.target)}
                 />
                 <span className="ml-2 text-gray-500">
                   {" "}
